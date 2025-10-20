871470a5fa20ed344f9b2bb845dc927335ab66d4
panic360 <jnaidu360@gmail.com>
2025-10-20 15:09:30 -0700

    Functional door in level 1

---
+++ Assets/Scripts/Door/DoorScript.cs
@@ -0,0 +1,25 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public BoxCollider2D col;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OpenDoor()
+    {
+        col.enabled = false;
+        transform.Find("Obscure").gameObject.SetActive(false);
+    }
+}
